f3efda39443e3bba6cee5114e4d4bd7dd4c37a9b
= <asaptorvi@protonmail.com>
2021-02-01 18:39:21 -0300

    fix: Added electron reload

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 const { app, BrowserWindow } = require('electron');
 
+require("electron-reload")(__dirname, {
+  electron: require(`${__dirname}/node_modules/electron`),
+});
+
 async function createWindow() {
   const win = new BrowserWindow({
     width: 1600,
     height: 900,
+    backgroundColor: '#2e2c29',
     webPreferences: {
       nodeIntegration: true
-    }
+    },
+    show: false
   })
-  
   win.loadFile('index.html');
+
+  win.once('ready-to-show', async () => {
+    win.show();
+  })
 }
 
 app.whenReady().then(createWindow)
